6e7921bd8d843c71a15ff90826c369fe0da55132
BelkinMax <belkinurban@gmail.com>
2020-10-26 19:22:35 +0100

    2-22 Union Types

--- app.ts
+++ app.ts
@@ -1,20 +1,15 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
-  name: 'Max',
-  age: 25,
-  hobbies: ['sports', 'cooking'],
-  role: [2, 'author']
-};
+function combine(input1: number | string, input2: number | string) {
 
-let favoriteArctivities: string[];
-favoriteArctivities = person.hobbies;
-
-console.log(person.name)
-
-for ( const hobby of favoriteArctivities ) {
-  console.log(hobby.toUpperCase())
+  let result: number | string;
+  if (typeof input1 === 'number' && typeof input2 === 'number') {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
 }
+
+const combineAges = combine( 30, 26 );
+const combineNames = combine( 'Max', 'Anna' );
+
+console.log(combineAges, combineNames)
