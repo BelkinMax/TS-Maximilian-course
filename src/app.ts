7212ae92b92ca79c251ec2877fd17e0becf8bb14
BelkinMax <belkinurban@gmail.com>
2020-11-01 07:46:04 +0100

    66. Protected

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 class Department {
-  private employees: string[] = [];
+  //private employees: string[] = []; // only can access from department class
+  protected employees: string[] = []; // can acces from Department class and all that extends it
 
   constructor(private readonly id: string, public name: string) {
-    // this.name = n;
+    this.id = id;
+    this.name = name;
   }
 
   describe() {
-    console.log(`Department (${this.id}): ${this.name}`)
+    console.log(`Department (${this.id}): ${this.name}`);
   }
 
   addEmployee(employee: string) {
@@ -19,10 +21,41 @@ class Department {
   }
 }
 
-const accounting = new Department('id1', 'Accounting');
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
+}
 
-accounting.addEmployee('Max');
-accounting.addEmployee('Anna');
+class AccountingDepartment extends Department {
+  reports: string[];
+  constructor(id: string, reports: string[]) {
+    super(id, "Accounting");
+    this.reports = reports;
+  }
 
-accounting.describe();
-accounting.printEmployeeInfo();
+  addReport(report: string) {
+    this.reports.push(report);
+  }
+}
+
+const itDep = new ITDepartment("id2", ["Bob"]);
+const accountingDep = new AccountingDepartment("id1", ["Report 1", "Report 2"]);
+
+accountingDep.addEmployee("Maxim");
+accountingDep.addEmployee("Anna");
+
+itDep.addEmployee("Bob");
+itDep.addEmployee("Sara");
+
+accountingDep.describe();
+accountingDep.printEmployeeInfo();
+itDep.describe();
+itDep.printEmployeeInfo();
+
+accountingDep.addReport("New report");
+
+console.log(itDep);
+console.log(accountingDep);
