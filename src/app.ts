65ff353a7707d1b7dac05cadebe5f404cb26c8a0
BelkinMax <belkinurban@gmail.com>
2021-03-26 12:59:29 +0100

    test

--- src/app.ts
+++ src/app.ts
@@ -1,82 +1,21 @@
-class Department {
-	protected employees: string[] = [];
+interface Person {
+	name: string;
+	age: number;
+	isMale: boolean;
 
-	constructor(private readonly id: number, private name: string) {}
-
-	get getDescription(): string {
-		return `
-    ID:                ${this.id};
-    Department:        ${this.name};
-    Employeees num.:   ${this.employees.length} employees;
-    `;
-	}
-
-	get getEmployees(): string[] {
-		return this.employees;
-	}
-
-	set setEmployee(employee: string) {
-		this.employees.push(employee);
-	}
+	greet(phrase: string): void;
 }
 
-class AccountingDepartment extends Department {
-	private managers: string[] = [];
+let user_1: Person;
 
-	constructor(id: number) {
-		super(id, 'Accounting');
-	}
+user_1 = {
+	name: 'Max',
+	age: 25,
+	isMale: true,
 
-	get getManagers() {
-		return this.managers;
-	}
+	greet(phrase) {
+		console.log(phrase + ' ' + this.name);
+	},
+};
 
-	set setManager(manager: string) {
-		this.managers.push(manager);
-	}
-}
-
-class ITDepartment extends Department {
-	private admins: string[] = [];
-
-	constructor(id: number) {
-		super(id, 'IT');
-	}
-
-	get getAdmins() {
-		return this.admins;
-	}
-
-	set setAdmin(admin: string) {
-		this.admins.push(admin);
-	}
-}
-
-const accounting = new AccountingDepartment(13);
-const it = new ITDepartment(12);
-
-/* -------------------------------------------------------------------------- */
-/*                                    LOGS                                    */
-/* -------------------------------------------------------------------------- */
-
-accounting.setEmployee = 'Max';
-accounting.setEmployee = 'Bob';
-accounting.setEmployee = 'Lara';
-accounting.setEmployee = 'Tom';
-
-accounting.setManager = 'Bob';
-accounting.setManager = 'Lara';
-
-it.setEmployee = 'Lorem';
-it.setEmployee = 'Ipsum';
-it.setEmployee = 'Random';
-
-it.setAdmin = 'Random';
-
-console.log(accounting.getDescription);
-console.log(accounting.getEmployees);
-console.log(accounting.getManagers);
-
-console.log(it.getDescription);
-console.log(it.getEmployees);
-console.log(it.getAdmins);
+user_1.greet('Hello');
