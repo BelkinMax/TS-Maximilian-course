c0db2e715ef1b02ecddc8eadf55850ed8654b798
BelkinMax <belkinurban@gmail.com>
2021-03-26 13:26:52 +0100

    function type

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,43 @@
-interface Person {
-	name: string;
-	age: number;
-	isMale: boolean;
+// type AddFn = (a: number, b: number) => number;
+interface NumbersFn {
+	(a: number, b: number): number;
+}
 
+const add: NumbersFn = (a, b) => {
+	return a + b;
+};
+
+const sub: NumbersFn = (a, b) => {
+	return a - b;
+};
+
+console.log('Add:', add(1, 2));
+
+interface Named {
+	readonly name: string;
+}
+
+interface Greetable extends Named {
 	greet(phrase: string): void;
 }
 
-let user_1: Person;
+class Person implements Greetable {
+	name: string;
+	age = 30;
 
-user_1 = {
-	name: 'Max',
-	age: 25,
-	isMale: true,
+	constructor(n: string) {
+		this.name = n;
+	}
 
-	greet(phrase) {
+	greet(phrase: string) {
 		console.log(phrase + ' ' + this.name);
-	},
-};
+	}
+}
 
-user_1.greet('Hello');
+let user1: Greetable;
+
+user1 = new Person('Max');
+// user1.name = 'Manu';
+
+user1.greet('Hi there - I am');
+console.log(user1);
